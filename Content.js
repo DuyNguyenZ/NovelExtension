798b7724a28ef65e19a1147b885cfa672d539231
DuyNguyenz <mansionnguyenz@gmail.com>
2024-01-09 11:04:45 -0700

    Yuna Deepl Version Automatic Tranlate

--- Content.js
+++ Content.js
@@ -1,47 +1,14 @@
 
-
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-      if( request.message === "start" ) {
-        copyDiv();
-        }else if(request.message === "dopass") {
-        chrome.storage.local.get(['mydata'], function(data) {           
-         
-        var lines = data.mydata.split("\n");
-        pasteData(lines);
-
-        chrome.storage.local.remove('mydata',function() {});
-        });       
-        }
-    });
-
-
-function copyDiv() {
-    let range = document.createRange();
-    let referenceNode = document.getElementById("novel_honbun");
-    range.selectNode(referenceNode);
-    window.getSelection().removeAllRanges(); 
-    window.getSelection().addRange(range); 
-    document.execCommand("copy");
-    window.getSelection().removeAllRanges();
-}
-
-
-function pasteData(line) {   
-var linez = line; 
-var element = document.querySelector('.novel_view');
-document.getElementById("novel_honbun").innerHTML = "  ";
-for(var i = 0 ; i < linez.length ; i++)
-{
-    var pElem = document.createElement('p');
-    var space = document.createElement('p');
-    pElem.innerHTML = linez[i];
-         if(linez[i] == "")
-        {
-            space.innerHTML = "&nbsp;";
-            element.appendChild(space);
-        }
-    element.appendChild(pElem);
-}
-}
+      //start call the Translate function to translate the web novel data 
+      if( request.message === "start" ) {             
+        // Get the data from the Novel Page
+        var novelData = document.getElementById("novel_honbun").innerText;
+        //Send Back the Novel Data to Popup Page
+        sendResponse({"Data": novelData });
+      }else{
+        document.getElementById("novel_honbun").innerText = request.message;
+      }
+  });
 
--- popup.js
+++ popup.js
@@ -1,32 +1,67 @@
+//Popup.js where will do the request tranlate to deepl and send it back to Content.js  
 
-//===================================
-function popup() {
-    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "start"});
-   });
-}
-
-
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("CopyZ").addEventListener("click", popup);
+//When the Trnaslate button onClick it will make the request to Content.js for Novel Data and start tranlate it. Then send the tranlate text back 
+$(document).ready(function() {
+  // Attach a click event listener to the Translate button
+  $("#Translate").on("click", startTranlate);
 });
 
-//========================================================================
-
-
-function popup2() {
-    var textArea = document.getElementById("texta").value;
-     
-    chrome.storage.local.set({'mydata': textArea}, function() { });
-
-
-    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+//Function for communicate with the Content.js for request the novel data 
+function startTranlate() {
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
     var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "dopass"});
-   });
+    
+    // Send a message to the content script to start the translation
+    chrome.tabs.sendMessage(activeTab.id, { "message": "start" }, function (response) {
+      translateNovelPage(response.Data);
+    });
+  });
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-document.getElementById("PasteZ").addEventListener("click", popup2);
-});
+//function for send the message/data back to Content.js after finish tranlasted
+function tranlatedMessage(message){
+  var messagez = message; 
+  chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {"message": messagez});
+  });
+}
+
+//Request to Deepl api for tranlate the novel
+async function translateNovelPage(DataNovel) {  
+
+  //api key
+  const apiKey = "[3fcd8b27-c555-1d7f-6712-3061c0ec5fae]";
+  const targetLang = "EN";
+  const novelData = DataNovel;
+
+  const payload = {
+    text: [novelData],
+    target_lang: targetLang
+  };
+
+  try {
+    const response = await fetch("https://api-free.deepl.com/v2/translate", {
+      method: "POST",
+      headers: {
+        "Authorization":  "DeepL-Auth-Key 3fcd8b27-c555-1d7f-6712-3061c0ec5fae:fx",
+        "User-Agent": "YunaNove/1.0" ,
+        "Content-Length": 100,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      const translatedText = data.translations[0].text;
+      console.log(translatedText);
+      tranlatedMessage(translatedText);
+    } else {
+      tranlatedMessage(response.statusText);
+    }
+  } catch (error) {
+    tranlatedMessage(error);
+  }
+}
+
